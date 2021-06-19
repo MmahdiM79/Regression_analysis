7d2ee38286e2b0ce166248837ff4f3fcc17ec3a0
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-19 15:58:40 +0430

    y and d matrix calculated

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ import matplotlib.pyplot as plt
 
 
 
+def D_ij(i: int, j: int) -> int:
+
+    if i == j:
+        return 1
+    if j - i == 1:
+        return -1
+    
+    return 0
+
+
+
 
 
 if __name__ == '__main__':
 
     btcusdt_chart = np.load('btc_price.npy')
-    
+
+
+    y = np.zeros(shape=btcusdt_chart.shape)
+
+    D = np.array([[D_ij(i, j) for j in range(len(y))] for i in range(len(y)-1)])
+
 
 
